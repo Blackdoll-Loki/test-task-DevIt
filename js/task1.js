306a8dd25ac78832a3969a424e6e4f0185853afb
Blackdoll-Loki <helpmeloki7@gmail.com>
2024-05-06 22:50:35 +0300

    fix task1 and task6 solution

--- js/task1.js
+++ js/task1.js
@@ -1,14 +1,14 @@
 function nodeChildCount(node, deep = Infinity){
   let amountOfChildren = 0;
   let deepCounter = 0;
-  if(node.children.length === 0){
+  if(node.children.length === 0 || deep === 0){
     return amountOfChildren;
   } else {
     amountOfChildren += node.children.length;
     deepCounter++;
     if(deepCounter < deep){
       for (let child of node.children){
-        return  amountOfChildren += nodeChildCount(child);
+        return  amountOfChildren += nodeChildCount(child, deep);
       }
     } else {
       return amountOfChildren;
--- js/task6.js
+++ js/task6.js
@@ -1,6 +1,5 @@
 function multiplyOddOnMin(arr){
   let min  = Math.min(...arr.flat());
-  console.log(min)
   let resultArr = arr.map(subArr => {
     return subArr.map((el)=>{
       if(el%2 === 1){
